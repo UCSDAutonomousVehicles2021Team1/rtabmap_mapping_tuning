85ffbd8de504575e6acb92bc3418746534e13096
sisaha9 <sisaha@ucsd.edu>
2020-11-15 17:34:00 -0800

    Added everything

--- src/etl.py
+++ src/etl.py
@@ -1,8 +1,10 @@
 import os
-import numpy as np
+import subprocess
+import glob
 import pandas as pd
 
 
 def convert_data(rawdir, mapdir, posedir):
-
+    print(glob.glob(rawdir))
+#     subprocess.call(["rtabmap-report", "--poses" rtabmap.db])
     print(rawdir, mapdir, posedir)
