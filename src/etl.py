5d8d782cdf105a06541eff92c418f35bd80b003f
sisaha9 <49205122+sisaha9@users.noreply.github.com>
2020-12-10 12:28:57 -0800

    Update etl.py

--- src/etl.py
+++ src/etl.py
@@ -4,20 +4,21 @@ import glob
 import numpy as np
 
 def move_data(rawdir, posedir):
-	"""
-	>>> posedir = "data/poses/"
-	>>> os.path.isfile(posedir+'param1_gt.txt')
-	True
-	>>> os.path.isfile(posedir+'param2_gt.txt')
-	True
-	>>> os.path.isfile(posedir+'param1_slam.txt')
-	True
-	>>> os.path.isfile(posedir+'param1_odom.txt')
-	True
-	"""
-	for file in set(np.apply_along_axis(lambda x: x[0][:x[0].find('_')], 0, np.array([os.listdir(rawdir)]))):
-		os.makedirs(posedir, exist_ok = True)
-		mv_dir_gt = subprocess.call(["cp", os.path.join(rawdir + '/' + file + '_gt.txt'), posedir])
-		mv_dir_slam = subprocess.call(["cp", os.path.join(rawdir + '/' + file + '_slam.txt'), posedir])
-		mv_dir_odom = subprocess.call(["cp", os.path.join(rawdir + '/' + file + '_odom.txt'), posedir])
-		mv_dir_yaml = subprocess.call(["cp", os.path.join(rawdir + '/' + file + '.yaml'), posedir])
+    """
+    >>> posedir = "data/poses/"
+    >>> os.path.isfile(posedir+'param1_gt.txt')
+    True
+    >>> os.path.isfile(posedir+'param2_gt.txt')
+    True
+    >>> os.path.isfile(posedir+'param1_slam.txt')
+    True
+    >>> os.path.isfile(posedir+'param1_odom.txt')
+    True
+    """
+    for file in list(filter(lambda x: x.endswith('.yaml'), os.listdir(rawdir))):
+        os.makedirs(posedir, exist_ok = True)
+        file = file[:-5]
+        mv_dir_gt = subprocess.call(["cp", os.path.join(rawdir + '/' + file + '_gt.txt'), posedir])
+        mv_dir_slam = subprocess.call(["cp", os.path.join(rawdir + '/' + file + '_slam.txt'), posedir])
+        mv_dir_odom = subprocess.call(["cp", os.path.join(rawdir + '/' + file + '_odom.txt'), posedir])
+        mv_dir_yaml = subprocess.call(["cp", os.path.join(rawdir + '/' + file + '.yaml'), posedir])
